b5f7b1e1076128c4072559f5ff7e8effcb3bb16f
wooknick <wooknick@naver.com>
2022-01-25 20:46:40 +0900

    "feat" basic frontend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
 
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(3000, () => {
+  console.log("Listening on https://localhost:3000");
+});
