1996505fd2c623326a451f684fbdd3836f68ed22
wooknick <wooknick@naver.com>
2022-01-25 22:55:53 +0900

    "feat" simple chat function

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { WebSocketServer } from "ws";
+import http from "http";
 import path from "path";
 
 const app = express();
@@ -9,6 +11,31 @@ app.set("views", __dirname + "/src/views");
 app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000, () => {
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  socket["nickname"] = "Anonymous";
+  sockets.push(socket);
+  console.log("Connected to Browser");
+  socket.on("close", () => {
+    console.log("closed connection");
+  });
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString());
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((s) =>
+          s.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
+
+server.listen(3000, () => {
   console.log("Listening on https://localhost:3000");
 });
